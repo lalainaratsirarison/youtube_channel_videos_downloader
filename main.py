a11a5bf8572b0f324f55fab719b629df0ab38810
Lalaina Ratsirarison <lalainaratsirarison@gmail.com>
2025-04-19 19:39:17 +0300

    second commit

--- main.py
+++ main.py
@@ -1,79 +1,11 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
-import time
-from pytube import YouTube
+from utils import *
 
-def scrap_it(channel_url):
-    options = Options()
-    options.headless = True
-    options.add_argument("--start-maximized")
-    driver = webdriver.Chrome(service=Service(), options=options)
-    driver.get(f'{channel_url}/videos')
-
-    prev_height = 0
-    footer = driver.find_element(By.ID, "footer")
-
-    while True:
-        delta_y = footer.rect['y']
-        ActionChains(driver).scroll_by_amount(0, delta_y).perform()
-        time.sleep(5)
-        
-        new_height = driver.execute_script("return document.documentElement.scrollHeight")
-        if prev_height == new_height:
-            print("Fin du chargement")
-            print(new_height)
-            break
-        prev_height = new_height
-
-    contents_container = driver.find_element(By.ID, "contents")
-    contents = contents_container.find_elements(By.ID, "video-title-link")
-    videos_list = set()
-    for content in contents:
-        videos_list.add({
-            "title": content.get_attribute("title"),
-            "link": content.get_attribute("href")
-        })
-        
-    driver.quit()
-    
-
-def download_it(video_desc):
-    yt = YouTube(f"https://https://www.youtube.com/watch?v={video_desc}")
-    print(f"Titre {yt.title}")
-    print("Résolutions disponible:\n")
-    stream = yt.streams.get_highest_resolution()
-    try:
-        print(f"{video_desc.title}\nTéléchargement en cours...")
-        stream.download(output_path="/videos")
-        print("Téléchargement terminé")
-    except:
-        print("Une erreur est survénue")
-
-
-def download_all(videos):
-    for video in videos:
-        download_it(video)
-
-
-def download_partial(videos, choosen_videos):
-    for video in choosen_videos:
-        download_it(videos[video])
-
-
-def get_videos_numbers(choices):
-    videos_list = set()
-    # if(choices.split(",") )
-
-
-if __name__ == '__main__':
-    channel = input("Coller le lien de la chaine Youtube ici: ") + "/videos"
-    videos = scrap_it(channel)
-    print(videos)
-    # print("Choisir les vidéos à télécharger:\n"
-    #     "(exemple: 1 ou 1-9 ou 1,2,5,6)\n")
-    # for video in videos:
-    #     print(f"{videos.index(video)+1} - {video.title}")
-    # choices = input()
+channel = input("Paste the channel URL: ")
+channel = channel.strip()
+videos = scrap_it(channel)
+print(videos)
+user_choices = split_input(input("Choose the videos you want to download (1,2,3... or 1-5 etc.): "), videos)
+download(user_choices)
+print("Thank you for using this program!")
+print("Have a nice day!")
+print("Lalaina Ratsirarison")
---
+++ utils.py
@@ -0,0 +1,80 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+from pytube import YouTube
+
+
+def scrap_it(channel_url):
+    options = Options()
+    options.add_argument("--headless=new")
+    options.add_argument("--start-maximized")
+    driver = webdriver.Chrome(service=Service(), options=options)
+    driver.get(f'{channel_url}/videos')
+
+    prev_height = 0
+    footer = driver.find_element(By.ID, "footer")
+
+    while True:
+        delta_y = footer.rect['y']
+        ActionChains(driver).scroll_by_amount(0, delta_y).perform()
+        time.sleep(5)
+        
+        new_height = driver.execute_script("return document.documentElement.scrollHeight")
+        if prev_height == new_height:
+            print(new_height)
+            break
+        prev_height = new_height
+
+    contents_container = driver.find_element(By.ID, "contents")
+    contents = contents_container.find_elements(By.ID, "video-title-link")
+    videos_list = set()
+    for content in contents:
+        videos_list.add({
+            "title": content.get_attribute("title"),
+            "link": content.get_attribute("href")
+        })
+        
+    driver.quit()
+
+
+def print_videos(videos):
+    for i, video in enumerate(videos):
+        print(f"{i + 1} - {video.title}\n")
+
+
+def download(videos):
+    video_count = 1
+    for video in videos:
+        yt = YouTube(f"https://https://www.youtube.com/watch?v={video.link}")
+        print(f"({video_count + 1} / {len(videos)}) - {yt.title}")
+        stream = yt.streams.get_highest_resolution()
+        try:
+            print(f"{video.title}\nDownloading...")
+            stream.download(output_path="/videos")
+            print("Download complete")
+        except:
+            print("An error occured while downloading")
+
+
+def split_input(user_input, choices):
+    result = []
+    parts = user_input.split(",")
+
+    for part in parts:
+        part = part.strip()
+
+        if "-" in part:
+            start_num, end_num = part.split("-")
+            start_num = int(start_num)
+            end_num = int(end_num)
+
+            for i in range(start_num, end_num + 1):
+                if i > len(choices):
+                    break
+                result.append(choices[i - 1])
+        else:
+            result.append(choices[int(part) - 1])
+    return result
